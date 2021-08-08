b6c5de8ccdfca5e89a10ad69c5cee792d03bda38
korodoff <rudoffsutnga368@gmail.com>
2021-08-08 19:19:14 +0530

    v1 starting script

---
+++ randomjoke.js
@@ -0,0 +1,32 @@
+console.log("Hello!.welcome.");
+console.log("press enter to play");
+let x = Math.floor((Math.random() * 10) + 1 );
+switch (x) {
+    case x=1:
+        console.log("My wife told me to stop impersonating a flamingo. I had to put my foot down. ");
+        break;
+    case x=2:
+        console.log("I went to buy some camo pants but couldn’t find any.");
+        break;
+    case x=3:
+        console.log(" I failed math so many times at school, I can’t even count.");
+        break;
+    case x=4:
+        console.log(" I used to have a handle on life, but then it broke.");
+        break;
+    case x=5:
+        console.log("I was wondering why the frisbee kept getting bigger and bigger, but then it hit me.");
+        break;
+    case x=6:
+        console.log("I heard there were a bunch of break-ins over at the car park. That is wrong on so many levels.");
+        break;
+    case x=7:
+        console.log("When life gives you melons, you might be dyslexic.");
+        break;
+    case x=8:
+        console.log("Don’t you hate it when someone answers their own questions? I do.");
+        break;
+    default:
+        console.log("you are a joke")
+        break;
+};
